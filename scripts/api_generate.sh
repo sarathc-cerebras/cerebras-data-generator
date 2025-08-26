9280e4eb886836c2ad0938408d16c3115efd8bea
sarathc-cerebras <sarath.chandran@cerebras.net>
2025-08-26 19:21:00 +0400

    adds http support and fixed low RPM batching issue

---
+++ scripts/api_generate.sh
@@ -0,0 +1,6 @@
+python src/api_client.py \
+    --dataset-repo "BAAI/Infinity-Instruct" \
+    --dataset-subset "7M_core" \
+    --dataset-split "train" \
+    --n-samples 2 \
+    --max-concurrency 1
